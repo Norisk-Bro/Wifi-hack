250ce0a01fd220a88bf9b1dab96e9b4dd450ecc3
༒⃝⃝༆Sᴀᴊɪʙ❤️⃟⃟⃟⃟⃟℠⃟⃟༒ <sajibmeta1day@gmail.com>
2026-08-28 16:59:54 +0600

    Update Wifi-hack.py

--- Wifi-hack.py
+++ Wifi-hack.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-# Sajib.py — ঠিক করা সংস্করণ
-# script.py-তে এখন main() আছে; import করলেই পুরো টুল চলে
 import sys
-import script
-
+import Sajibx
 if __name__ == '__main__':
-    sys.exit(script.main())
+    sys.exit(Sajibx.main())
